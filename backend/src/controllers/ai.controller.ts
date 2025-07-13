242b10317edca4483fc5ced6cadd07e3bf44cc7a
Shan Qing <sqing73@gmail.com>
2025-07-13 00:47:47 -0700

    add support for dynamic images

--- backend/src/controllers/ai.controller.ts
+++ backend/src/controllers/ai.controller.ts
@@ -1,21 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import { getTaskExtraction, getTaskImage, getTaskImageMapping } from "@/services/ai.service";
+import { getActivityExtraction, getTaskImage } from "@/services/ai.service";
 
-export async function handleTaskExtractionRequest(req: Request, res: Response, next: NextFunction) {
+export async function handleActivityExtractionRequest(req: Request, res: Response, next: NextFunction) {
   try {
-    const { prompt } = req.body;
-    const completion = await getTaskExtraction(prompt);
+    const { prompt, images } = req.body;
+    const completion = await getActivityExtraction({prompt, images});
     if (!completion) {
       return res.status(400).json({ error: "Prompt is not valid, please try again!" });
     }
     return res.json(completion);
   } catch (error) {
+    console.log(error);
     next(error);
     return;
   }
 }
 
-export async function handleTaskImageRequest(req: Request, res: Response, next: NextFunction) {
+export async function handleActivityImageGenerationRequest(req: Request, res: Response, next: NextFunction) {
   try {
     res.setHeader("Content-Type", "text/plain");
     
@@ -27,20 +28,6 @@ export async function handleTaskImageRequest(req: Request, res: Response, next: 
     }
     
     res.send(imageBinary);
-  } catch (error) {
-    next(error);
-  }
-  return;
-}
-
-export async function handleTaskImageMappingRequest(req: Request, res: Response, next: NextFunction) {
-  try {
-    const { prompt } = req.body;
-    if (!prompt) {
-      return res.status(400).json({ error: "Prompt is required" });
-    }
-    const imageName = await getTaskImageMapping(prompt);
-    return res.json({ imageName });
   } catch (error) {
     next(error);
   }
